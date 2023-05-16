15de8ddc7f52fd90b5afcd2abf579e868d77a92e
Vincent Vermeire <vvermeire55@gmail.com>
2023-05-16 10:45:39 +0200

    Removed text from homescreen

--- src/View/Pages/Home.tsx
+++ src/View/Pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1 className='text-4xl font-bold text-center'>Home</h1>
+            <h1 className='text-4xl font-bold text-center'></h1>
         </div>
     )
 }
