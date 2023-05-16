ba09765f7a20876abc15b9797025cdd826ef21a6
Vincent Vermeire <vvermeire55@gmail.com>
2023-05-16 20:02:56 +0200

    removed unecessary import

--- src/View/Components/UserInfo.tsx
+++ src/View/Components/UserInfo.tsx
@@ -1,5 +1,4 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { ReactComponent as AccountIcon } from '../Images/AccountIcon.svg'
 import LoginButton from "./LoginButton";
 import ProfilePopover from "./ProfilePopover";
 
