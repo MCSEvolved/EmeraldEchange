d7499bec2a69c2dcb89f78046b40112ae0e9a25b
Vincent Vermeire <vvermeire55@gmail.com>
2023-05-16 16:59:13 +0200

    added dummy logos to portal

--- src/View/Components/Portal.tsx
+++ src/View/Components/Portal.tsx
@@ -1,5 +1,11 @@
 import { Popover, Transition } from "@headlessui/react";
 import {ReactComponent as PortalGridIcon} from '../Images/PortalGridIcon.svg'
+import {ReactComponent as Asset_9} from '../Images/Asset 9.svg'
+import {ReactComponent as Asset_10} from '../Images/Asset 10.svg'
+import {ReactComponent as Asset_11} from '../Images/Asset 11.svg'
+import {ReactComponent as Asset_12} from '../Images/Asset 12.svg'
+import {ReactComponent as Asset_13} from '../Images/Asset 13.svg'
+
 
 export default function Portal() {
 
@@ -7,12 +13,11 @@ export default function Portal() {
     const portalIcon = document.getElementById('portal-icon')
     if(!portalIcon) return
     portalIcon.classList.toggle('rotate-180')
-    console.log('rotate')
   }
 
   return (
     <Popover className='relative'>
-      <Popover.Button className='h-max p-3 rounded-full outline-0 hover:bg-white hover:bg-opacity-20 active:bg-opacity-25'>
+      <Popover.Button className='h-max p-3 rounded-full outline-0 hover:bg-white hover:bg-opacity-20 active:bg-opacity-25 duration-300'>
         <PortalGridIcon id="portal-icon" className='h-7 w-auto duration-700'></PortalGridIcon>
       </Popover.Button>
 
@@ -28,7 +33,11 @@ export default function Portal() {
       >
         <Popover.Panel>
           <div className='absolute bg-MCS-DarkerBlue rounded-br-xl h-96 w-52'>
-            <a>test</a>
+             <Asset_9 className='h-8 w-auto mb-4 mt-2 mx-2'></Asset_9>
+            <Asset_10 className='h-8 w-auto mb-4 mt-2 mx-2'></Asset_10>
+            <Asset_11 className='h-8 w-auto mb-4 mt-2 mx-2'></Asset_11>
+            <Asset_12 className='h-8 w-auto mb-4 mt-2 mx-2'></Asset_12>
+            <Asset_13 className='h-8 w-auto mb-4 mt-2 mx-2'></Asset_13>
           </div>
         </Popover.Panel>
       </Transition>
